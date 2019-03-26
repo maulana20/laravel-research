ea96dc6a6eee70068c1e24bc687d83bb46b41b8d
maulana <maulana20@versatech.co.id>
2019-03-26 12:04:10 +0700

    masih belum bisa
    Signed-off-by: maulana <maulana20@versatech.co.id>

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -3,30 +3,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use App\Parents;
 
 class CheckRole
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-		$parents = new Parents();
+	public function handle($request, Closure $next)
+	{
+		$roles = $this->checkRoute($request->route());
 		
-        $roles = $this->checkRoute($request->route());
-        
-        if ($parents->hasRole($roles) || !$roles) return $next($request);
-        
-        return abort(503, 'Anda tidak memiliki hak akses');
-    }
-    
-    public function checkRoute($route)
-    {
+		if ($parents->hasRole($roles) || !$roles) return $next($request);
+		
+		return abort(503, 'Anda tidak memiliki hak akses');
+	}
+	
+	public function checkRoute($route)
+	{
 		$actions = $route->getAction();
 		
 		return isset($actions['roles']) ? $actions['roles'] : null;
--- app/Role.php
+++ app/Role.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
 	protected $table = 'tblProfile';
+	protected $primaryKey = 'profile_code';
 }
